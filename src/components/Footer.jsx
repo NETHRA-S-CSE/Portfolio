912460b679b388d6a5818497d9c77b823ef40113
NETHRA S <s.nethra2024cse@sece.ac.in>
2026-01-08 16:34:34 +0530

    Initial Project Structure Created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer()
+{
+    return (
+        <>
+        <footer>
+            <p>Portfolio of Nethra S</p>
+            <h3>Contact :</h3>
+            <ul>
+                <li>Email: s.nethra2024cse@sece.ac.in</li>
+                <li>Phone: +91 72000 16353</li>
+                <li>Linkedin : </li>
+            </ul>
+        </footer>
+        </>
+    )
+}
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header()
+{
+    return (
+        <>
+        <header>
+            <h1>Hello! This is Nethra..!</h1>
+            <h3>Welcome to my portfolio</h3>
+
+            <nav>
+                <a href="#about">About Me</a>
+                <a href="#skills">Skills</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </nav>
+
+        </header>
+        </>
+    )
+}
+export default Header;
